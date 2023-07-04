ccf1a8f5f9e971cdbdecfa724680ee57c48c6e14
Amaz1ng <106401502+amz1ng@users.noreply.github.com>
2023-07-04 23:16:36 +0300

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+ 
+    DWORD processId;
+    cout << "Enter the process ID: ";
+    cin >> processId;
+ HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
+ cout << "Enter the value: ";
+ int value = 0;
+ cin >> value;
+ cout << "Enter offset: ";
+ LPVOID offset;
+ cin >> offset;
+    
+    int WriteStatus = WriteProcessMemory(process, (LPVOID)offset, &value, (DWORD)sizeof(value), 0);
+    CloseHandle(process);
+    return 0;
+
+}
